84c1d9f960775b8d232512567bebc2d9103c70fd
Vinicius <paulo.vinicius3422@gmail.com>
2023-07-25 02:39:31 -0300

    finished portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,8 +20,60 @@ function linkAction() {
     this.classList.add("active");
 
     const navMenu = document.getElementById("nav__menu");
-    navMenu.classList.remove("open")
+    navMenu.classList.remove("open");
 };
 
 
 navLink.forEach(navLink => navLink.addEventListener("click", linkAction));
+
+
+/*=== SCROLL REVEAL ANIMATION ===*/
+
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: "80px",
+    duration: 2000,
+    reset:true
+});
+
+
+
+/*=== SCROLL HOME ===*/
+
+sr.reveal('.home__title',{});
+sr.reveal('.button',{delay:200});
+sr.reveal('.home__img', {delay:400});
+sr.reveal('.home__social-icon', {interval:200})
+
+/*=== SCROLL ABOUT ===*/
+
+sr.reveal('.about__img',{});
+sr.reveal('.about__subtitle',{delay:200});
+sr.reveal('.about__text',{delay:400});
+
+
+/*=== SCROLL SKILLS ===*/
+
+sr.reveal('.experience-sub-title',{delay:200});
+sr.reveal('.animation-container',{delay:200});
+sr.reveal('.skills__social-icon',{interval:200});
+
+/*=== SCROLL WORKS ===*/
+
+sr.reveal('.color-container',{delay:200});
+sr.reveal('.article-contianer', {delay:200});
+sr.reveal('.project-title', {delay:300});
+
+
+/*=== SCROLL CONTACT ===*/
+
+sr.reveal('.contact__container', {delay:200});
+sr.reveal('.contact-info-cotainer', {delay:400});
+sr.reveal('.animation-icon', {interval:300})
+sr.reveal('.animation-info', {delay:400})
+
+/*=== SCROLL FOOTER ===*/
+
+sr.reveal('.footer__title',{delay:200});
+sr.reveal('.ul__links',{interval:100})
